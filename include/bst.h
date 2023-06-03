5743f1b1e8fba4e29c0b8d5e8c7a2f56287993f8
LesyaVasilek <125215125+LesyaVasilek@users.noreply.github.com>
2023-06-03 17:47:37 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -3,7 +3,7 @@
 #define INCLUDE_BST_H_
 template <typename T>
 class BST {
-public:
+ public:
     struct Node {
         Node* right;
         Node* left;
@@ -11,7 +11,7 @@ public:
         int counter;
     };
 
-private:
+ private:
     Node* root;
     Node* adNode(Node* root, T value_element) {
         if (root == nullptr) {
@@ -20,8 +20,7 @@ private:
             root->counter = 1;
             root->left = nullptr;
             root->right = nullptr;
-        }
-        else {
+        } else {
             if (value_element < root->value) root->left = adNode(root->left, value_element);
             else if (value_element > root->value) root->right = adNode(root->right, value_element);
             else
@@ -43,14 +42,12 @@ private:
     int heightTree(Node* root) {
         if (root == nullptr) {
             return 0;
-        }
-        else {
+        } else {
             int left1 = heightTree(root->left);
             int right1 = heightTree(root->right);
             if (right1 > left1) {
                 return ++right1;
-            }
-            else {
+            } else {
                 return ++left1;
             }
         }
