5194c15cdb0bb980523087edce52d7f7a9aacc57
LesyaVasilek <125215125+LesyaVasilek@users.noreply.github.com>
2023-06-03 18:12:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -22,7 +22,7 @@ BST<std::string> makeTree(const char* filename) {
             else
                 break;
         }
-        tree.adNode(value);
+        tree.adNode_(value);
         value = "";
     }
     return tree;
